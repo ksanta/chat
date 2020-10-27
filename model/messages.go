be3a45813995172ba344547d14dc12314ee68415
Karl Santa <Karl.XY.Santa@iag.com.au>
2020-10-27 22:49:15 +1100

    Pass user room into backend store

--- model/messages.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Message struct {
-	// The action to take
-	Message string `json:"message"`
-	// The contents of the message
-	Data string `json:"data"`
-}
--- static/app.js
+++ static/app.js
@@ -1,16 +1,20 @@
-window.onload = function () {
-    var conn;
-    var msg = document.getElementById("msg");
-    var log = document.getElementById("log");
+// The Websocket connection
+let conn;
+// The room name, or blank for the global room
+let roomName = "";
 
-    function appendLog(item) {
-        var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-        log.appendChild(item);
-        if (doScroll) {
-            log.scrollTop = log.scrollHeight - log.clientHeight;
-        }
+let msg = document.getElementById("msg");
+let log = document.getElementById("log");
+
+function appendLog(item) {
+    let doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+    log.appendChild(item);
+    if (doScroll) {
+        log.scrollTop = log.scrollHeight - log.clientHeight;
     }
+}
 
+window.onload = function () {
     document.getElementById("form").onsubmit = function () {
         if (!conn) {
             return false;
@@ -20,35 +24,13 @@ window.onload = function () {
         }
 
         let msgToSend = {
-            message: "sendmessage",
+            type: "sendmessage",
             data: msg.value
         }
         conn.send(JSON.stringify(msgToSend));
         msg.value = "";
         return false;
     };
-
-    if (window["WebSocket"]) {
-        conn = new WebSocket("wss://3fewjvg7q8.execute-api.ap-southeast-2.amazonaws.com/Prod");
-        conn.onclose = function (evt) {
-            var item = document.createElement("div");
-            item.innerHTML = "<b>Connection closed.</b>";
-            appendLog(item);
-        };
-        conn.onmessage = function (evt) {
-            var messages = evt.data.split('\n');
-            for (var i = 0; i < messages.length; i++) {
-                var item = document.createElement("div");
-                item.innerText = messages[i];
-                appendLog(item);
-            }
-        };
-    } else {
-        var item = document.createElement("div");
-        item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-        appendLog(item);
-    }
-
 };
 
 $(document).ready(function () {
@@ -57,10 +39,61 @@ $(document).ready(function () {
 });
 
 // Make room text box active when the modal shows
+// todo: else if websockets is not supported, alert the user to that instead
 $('#room-modal').on('shown.bs.modal', function () {
     $('#room-name').trigger('focus')
 })
 
-// todo: Entering global room... when user doesn't specify room
+function connectWebSocket() {
+    conn = new WebSocket("wss://3fewjvg7q8.execute-api.ap-southeast-2.amazonaws.com/Prod");
+    registerWebsocketHandlers()
+}
 
-// todo: Entering "foobar" room... when user specifies room
+// User enters a room
+$("#room-form").submit(function (event) {
+    event.preventDefault();
+    connectWebSocket()
+    roomName = $('#room-name').val()
+    let item = document.createElement("div");
+    item.innerHTML = "<b>Joined " + roomName + " room</b>";
+    appendLog(item)
+    $('#room-modal').modal('hide')
+    $('#msg').focus()
+});
+
+// User enters the global room
+$('#room-modal').on('hidden.bs.modal', function (event) {
+    event.preventDefault();
+    if (roomName === "") {
+        connectWebSocket()
+        let item = document.createElement("div");
+        item.innerHTML = "<b>Joined global room</b>";
+        appendLog(item)
+        $('#msg').focus()
+    }
+})
+
+function registerWebsocketHandlers() {
+    // Register the user as soon as the socket connection is made
+    conn.onopen = function (evt) {
+        console.log("Registering user")
+        let msgToSend = {
+            type: "register",
+            roomName: roomName
+        }
+        conn.send(JSON.stringify(msgToSend));
+    }
+    conn.onclose = function (evt) {
+        let item = document.createElement("div");
+        item.innerHTML = "<b>Connection closed.</b>";
+        appendLog(item);
+    };
+    conn.onmessage = function (evt) {
+        let messages = evt.data.split('\n');
+        for (let i = 0; i < messages.length; i++) {
+            let item = document.createElement("div");
+            item.innerText = messages[i];
+            appendLog(item);
+        }
+    };
+}
